0682cd2b83e249e3b1ae3e9979b5b909ac7b35d6
constW <37398787+constW@users.noreply.github.com>
2019-12-26 09:38:32 +0800

    add
    add

---
+++ precache-manifest.e120398fb40cdc64afc726842fa69ec6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e88bbebd51c84f9c72f85e6624ad9d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3170a2b7b56f1006f73c",
+    "url": "/static/css/main.5712535d.chunk.css"
+  },
+  {
+    "revision": "c170504aa3b29c46b19a",
+    "url": "/static/js/2.1a34beea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1a34beea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3170a2b7b56f1006f73c",
+    "url": "/static/js/main.1f21bc00.chunk.js"
+  },
+  {
+    "revision": "bac79439103ec3f75727",
+    "url": "/static/js/runtime-main.bc77f0f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d0fc5d0c327b99c22b0aec7252f22a79",
+    "url": "/static/media/smzq.d0fc5d0c.png"
+  },
+  {
+    "revision": "5847053591c443b71fad92eea5eade8d",
+    "url": "/static/media/tj.58470535.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91a258ac30ad111928bb8103283af429.js"
+  "/precache-manifest.e120398fb40cdc64afc726842fa69ec6.js"
 );
 
 self.addEventListener('message', (event) => {
